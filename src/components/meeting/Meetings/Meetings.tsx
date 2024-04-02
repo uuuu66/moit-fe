3ab0c61866aeb3b8dc88fa0b9ec6d5c7960c7194
vjiji <restmi0711@gmail.com>
2024-04-02 14:01:43 +0900

    🚧 메인 미팅 리스트 작업중

---
+++ src/components/meeting/Meetings/Meetings.tsx
@@ -0,0 +1,14 @@
+export default function Meetings(): JSX.Element {
+  const meetings = [
+    {
+      meetingId: 1,
+      meetingName: '미팅이름1',
+      registeredCount: 3,
+      totalCount: 10,
+      firstSkillId: 2,
+      locationLat: 37.123,
+      locationLong: 128.123,
+    },
+  ]
+  return <div>meetingCard</div>
+}
