8a6097bacd86038e3c54ce745fdf278505e5c979
vjiji <restmi0711@gmail.com>
2024-04-02 11:42:00 +0900

    ✨ 카카오 지도 api사용 훅 생성 및 메인페이지 에서  지도 불러오기

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,33 @@
+import { Map } from 'react-kakao-maps-sdk'
+import styled from 'styled-components'
+import { useRef } from 'react'
+import useMap from '@/hooks/useMap'
+
 function Home(): JSX.Element {
-  return <div>Home</div>
+  useMap()
+
+  return (
+    <div>
+      <MainLayout>
+        <Map
+          center={{ lat: 37.5667, lng: 126.9784 }}
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+          level={10}
+          maxLevel={3}
+          minLevel={11}
+        />
+      </MainLayout>
+    </div>
+  )
 }
 
 export default Home
+
+const MainLayout = styled.div`
+  width: 360px;
+  height: 740px;
+  position: relative;
+`
