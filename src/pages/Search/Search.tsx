64c36e2081613a4c0ecdca00eb65c70af2301627
vjiji <restmi0711@gmail.com>
2024-04-04 14:15:46 +0900

    💄 스타일 관련 코드 분리 및 rem적용

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,8 +1,16 @@
 import { useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
-import styled from 'styled-components'
 import { meetingKeys } from '@/constants/queryKeys'
 import { getMeetingsBySearch } from '@/apis/meeting'
+import {
+  CardBox,
+  ContentsBox,
+  MeetingCard,
+  SearchLayout,
+  SectionBox,
+  SubContentsBox,
+  TagBox,
+} from './styles'
 
 export default function Search(): JSX.Element {
   const [searchText, setSearchText] = useState('')
@@ -63,65 +71,3 @@ export default function Search(): JSX.Element {
     </SearchLayout>
   )
 }
-
-const SearchLayout = styled.div`
-  width: 100%;
-  height: 100%;
-`
-const SectionBox = styled.div`
-  padding: 0 20px;
-
-  h1 {
-    margin-bottom: 20px;
-    font-weight: 600;
-  }
-`
-const CardBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  /* max-height: 300px; */ // Todo: 검색 바 크기에 따라 정해줄 것 (for 스크롤 적용)
-  overflow: auto;
-`
-const MeetingCard = styled.div`
-  width: 100%;
-  padding: 20px;
-  border-radius: 12px;
-  background: #e7e4e4;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-
-  hr {
-    width: 100%;
-  }
-`
-const ContentsBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-
-  h2 {
-    font-weight: 700;
-  }
-`
-
-const TagBox = styled.div`
-  display: flex;
-  gap: 6px;
-  font-size: 12px;
-
-  p {
-    padding: 6px 12px;
-    background: #fff;
-    border-radius: 25px;
-  }
-`
-
-const SubContentsBox = styled.div`
-  display: flex;
-  justify-content: space-between;
-  p {
-    font-size: 12px;
-  }
-`
