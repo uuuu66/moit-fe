64eecb744e3cc2497e966e0e38de118dc1319279
vjiji <restmi0711@gmail.com>
2024-04-15 20:59:56 +0900

    💄 공통으로 사용할 스타일 관련 상수 모듈을 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ThemeProvider } from 'styled-components'
 import Router from './shared/Router'
 import useResetToken from './hooks/useResetToken'
+import { theme } from './constants/theme'
 
 function App(): JSX.Element {
   const queryClient = new QueryClient()
@@ -9,7 +11,9 @@ function App(): JSX.Element {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
