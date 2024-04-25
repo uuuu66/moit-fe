eab5d4e7e8e5d8e349a16a3174cb14ddbf4fa156
LEE YEJIN <dldulwls@naver.com>
2024-04-25 22:18:28 +0900

    🔧수정,삭제 버튼 이동, 종료 시 버튼 추가

--- src/components/DetailHeader/DetailHeader.tsx
+++ src/components/DetailHeader/DetailHeader.tsx
@@ -1,7 +1,6 @@
 import { useLocation, useNavigate } from 'react-router-dom'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
-import { jwtDecode } from 'jwt-decode'
 import {
   ChatUsers,
   DetailHeaderContainer,
@@ -9,13 +8,9 @@ import {
   ToggleButton,
   ToggleContainer,
 } from './styles'
-import { deleteMeeting, getMeetingDetail } from '@/apis/meeting'
-import { MenuButton, MenuContainer } from '@/pages/Chat/styles'
+import { getMeetingDetail } from '@/apis/meeting'
 import { getLocalStorageItem } from '@/util/localStorage'
 import LoginModal from '../modals/LoginModal'
-import { notify } from '../Toast'
-import 'react-toastify/dist/ReactToastify.css'
-import AlertModal from '../modals/AlertModal'
 
 interface DetailHeaderProps {
   meetingId: number
@@ -25,29 +20,11 @@ function DetailHeader({ meetingId }: DetailHeaderProps): JSX.Element {
   const navi = useNavigate()
   const location = useLocation()
 
-  const [isOpen, setIsOpen] = useState(false)
   const [onLoginModal, setOnLoginModal] = useState(false)
-  const [onDeleteModal, setOnDeleteModal] = useState(false)
 
   const { data } = useQuery({
     queryKey: ['chatroom'],
     queryFn: async () => await getMeetingDetail(Number(meetingId)),
-  })
-
-  const deleteMutation = useMutation({
-    mutationFn: async () => {
-      await deleteMeeting(Number(meetingId))
-    },
-    onSuccess: () => {
-      notify({
-        type: 'default',
-        text: '모임이 삭제 되었습니다.',
-      })
-      navi('/')
-    },
-    onError: (error) => {
-      console.log('error', error)
-    },
   })
 
   const handleHomeClick = (): void => {
@@ -66,10 +43,6 @@ function DetailHeader({ meetingId }: DetailHeaderProps): JSX.Element {
     }
   }
 
-  const deleteMeetingClick = (): void => {
-    deleteMutation.mutate()
-  }
-
   const goback = (): void => {
     if (location.pathname === `/meetings/${meetingId}/chats`) {
       navi(`/meetings/${meetingId}`, { replace: true })
@@ -79,7 +52,6 @@ function DetailHeader({ meetingId }: DetailHeaderProps): JSX.Element {
   }
 
   const token: string = getLocalStorageItem('accessToken')
-  const decodedToken = token != null ? jwtDecode(token) : ''
 
   return (
     <DetailHeaderContainer>
@@ -119,68 +91,7 @@ function DetailHeader({ meetingId }: DetailHeaderProps): JSX.Element {
             />
           )}
         </ToggleContainer>
-        {decodedToken.sub === data?.creatorEmail ? (
-          <MenuButton
-            type="button"
-            aria-label="menu"
-            onClick={() => {
-              setIsOpen(!isOpen)
-            }}
-          >
-            <Icon src="/assets/menu.svg" alt="" className="menu" />
-          </MenuButton>
-        ) : (
-          <MenuButton
-            type="button"
-            aria-label="menu"
-            onClick={() => {
-              window.alert('개발 중인 기능입니다!')
-            }}
-          >
-            <Icon src="/assets/menu.svg" alt="" className="menu" />
-          </MenuButton>
-        )}
       </div>
-      {isOpen && decodedToken.sub === data?.creatorEmail && (
-        <MenuContainer>
-          <div>
-            <span>수정하기</span>
-            <button
-              type="button"
-              aria-label="edit"
-              onClick={() => {
-                navi(`/meetings/${meetingId}/modify`)
-              }}
-            >
-              <img src="/assets/edit.svg" alt="edit" />
-            </button>
-          </div>
-          <div>
-            <span>삭제하기</span>
-            <button
-              type="button"
-              aria-label="delete"
-              onClick={() => {
-                setOnDeleteModal(!onDeleteModal)
-              }}
-            >
-              <img src="/assets/delete.svg" alt="delete" />
-            </button>
-          </div>
-          {onDeleteModal && (
-            <AlertModal
-              message="삭제"
-              firstSubMessage="모임을 삭제하면"
-              secondSubMessage="관련 데이터가 모두 지워집니다."
-              onClose={() => {
-                setOnDeleteModal(!onDeleteModal)
-              }}
-              handleClick={deleteMeetingClick}
-              buttonName="삭제하기"
-            />
-          )}
-        </MenuContainer>
-      )}
     </DetailHeaderContainer>
   )
 }
