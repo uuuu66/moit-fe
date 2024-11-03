0661237776193b2fefdd5319d4463b01fab23edb
이민기 <uuuu66@naver.com>
2024-11-03 15:17:52 +0900

    build: aos 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
+import Aos from 'aos'
 import Router from './shared/Router'
 import useResetToken from './hooks/useResetToken'
 import { theme } from './constants/theme'
 import Toast from './components/Toast'
 import AuthProvider from './shared/AuthProvider'
+import 'aos/dist/aos.css'
 
 function App(): JSX.Element {
   const queryClient = new QueryClient({
@@ -18,7 +20,12 @@ function App(): JSX.Element {
     },
   })
   useResetToken()
-
+  Aos.init({
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease',
+  })
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
