a254827ec15a29e1765fbb4d38b5042a0c422841
이민기 <uuuu66@naver.com>
2024-11-03 12:42:40 +0900

    fix: 헤더 내용 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,57 @@
-import { Link, useLocation } from 'react-router-dom'
-import { memo } from 'react'
-import { ButtonBox, HeaderContainer } from './styles'
-import LoginButton from '../user/LoginButton/LoginButton'
+import { useLocation } from 'react-router-dom'
+import { memo, useState } from 'react'
+import { HeaderContainer } from './styles'
+import Input from '../common/Input/Input'
+import strings from '@/constants/strings'
+import SearchButton from './SearchButton'
+import FilterIcon from '@/svgs/filter.svg?react'
+import MoitIcon from '@/svgs/moit.svg?react'
+import FlexDiv from '../common/FlexDiv/FlexDiv'
+import TagFilter from '../common/TagFilter/TagFilter'
+import groupTypes from '@/constants/groupTypes'
 
 export default memo(function Header(): JSX.Element {
   const location = useLocation()
   const isHeaderRequired = location.pathname === '/'
-
+  const [selectedGrouptypes, setSelectedGroupTypes] = useState<
+    Array<keyof typeof strings.groupTypes>
+  >(['recommends'])
   return (
     <HeaderContainer $isShow={isHeaderRequired}>
-      <Link to="/">
-        <img src="/assets/logoHeader.svg" alt="logo" />
-      </Link>
-      <ButtonBox>
-        <Link to="/search">
-          <img src="/assets/search.svg" alt="search" />
-        </Link>
-        <LoginButton />
-      </ButtonBox>
+      <FlexDiv gap={8}>
+        <MoitIcon width={64} />
+        <Input
+          placeholder={strings.placeholders.search_placeholder}
+          suffixElement={<SearchButton />}
+          containerProps={{ style: { paddingRight: '4px', height: '40px' } }}
+        />
+        <FilterIcon width={32} height={32} />
+      </FlexDiv>
+      <FlexDiv gap={8}>
+        {Object.entries(groupTypes).map((grouptype) => (
+          <TagFilter
+            key={grouptype[0]}
+            isSelected={selectedGrouptypes.includes(grouptype[1].groupKey)}
+            selectedColor={grouptype[1].groupColor}
+            selectedBgColor={grouptype[1].groupBgColor}
+            onClick={() => {
+              if (!selectedGrouptypes.includes(grouptype[1].groupKey))
+                setSelectedGroupTypes([
+                  ...selectedGrouptypes,
+                  grouptype[1].groupKey,
+                ])
+              else
+                setSelectedGroupTypes(
+                  [...selectedGrouptypes].filter(
+                    (type) => type !== grouptype[1].groupKey
+                  )
+                )
+            }}
+          >
+            {grouptype[1].groupName}
+          </TagFilter>
+        ))}
+      </FlexDiv>
     </HeaderContainer>
   )
 })
